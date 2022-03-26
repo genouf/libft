41fa02e1b65c5e5aa4a01ef0f23b87dd8873e9d4
genouf <gabriel.enouf@yahoo.fr>
2022-03-26 22:10:03 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,13 +3,21 @@
 size_t  ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
     size_t  i;
+    size_t  j;
 
     i = 0;
     while (dst[i])
         i++;
     if (i >= dstsize)
-        return (ft_strlen(src) + size);
-
+        return ((size_t)ft_strlen(src) + dstsize);
+    j = 0;
+    while (src[j] && (i + j + 1 < dstsize))
+    {
+        dest[i + j] = src[j];
+        j++;
+    }
+    dest[i + j] = '\0';
+    return ((size_t)ft_strlen(src) + i);
 }
 
 int main(void)
