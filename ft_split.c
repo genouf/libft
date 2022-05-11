838641e83af2657a5988189e88f00eec86105c00
Gabriel ENOUF <genouf@e2r2p13.clusters.42paris.fr>
2022-05-11 12:30:53 +0200

    reecriture split (securisation)

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 10:12:19 by genouf            #+#    #+#             */
-/*   Updated: 2022/04/04 23:29:08 by genouf           ###   ########.fr       */
+/*   Updated: 2022/05/11 12:29:34 by genouf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,39 +25,56 @@ int	count_word(char const *s, char c)
 	return (count);
 }
 
-int	count_size(char const *s, char c, int i)
+char	**free_all(char **result, int x)
 {
-	int	count;
+	while (x > -1)
+		free(result[x--]);
+	free(result);
+	return (NULL);
+}
 
+char	*split_process(char const *s, char c, int *i)
+{
+	int		j;
+	int		count;
+	char 	*result;
+	
 	count = 0;
-	while (s[i] != c && s[i])
+	j = *i;
+	while (s[j] != c && s[j])
 	{
 		count++;
-		i++;
+		j++;
 	}
-	return (count + 1);
+	result = (char *)malloc(sizeof(char) * (count + 1));
+	if (result == NULL)
+		return (NULL);
+	(*i)--;
+	j = -1;
+	while (s[++(*i)] != c && s[*i])
+		result[++j]= s[*i];
+	result[j + 1] = '\0';
+	return (result);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
 	int		i;
-	int		j;
 	int		x;
 
 	i = 0;
 	x = -1;
 	result = (char **)malloc(sizeof(char *) * (count_word(s, c) + 1));
+	if (result == NULL)
+		return (free_all(result, x));
 	while (s[i])
 	{
 		if (s[i] != c)
 		{
-			j = -1;
-			result[++x] = (char *)malloc(sizeof(char) * count_size(s, c, i));
-			i--;
-			while (s[++i] != c && s[i])
-				result[x][++j] = s[i];
-			result[x][j + 1] = '\0';
+				result[++x] = split_process(s, c, &i);
+				if (result[x] == NULL)
+					return (free_all(result, x));
 		}
 		else
 			i++;
@@ -66,20 +83,20 @@ char	**ft_split(char const *s, char c)
 	return (result);
 }
 
-/*int	main(void)
-{
-	char	str[] = ("--1-2--3---4----5-----42");
-	char	**result;
-	int		i;
+// int	main(void)
+// {
+// 	char	str[] = ("--1-2--3---4----5-----42");
+// 	char	**result;
+// 	int		i;
 
-	result = ft_split(str, '-');
-	i = 0;
-	while (i < 7)
-	{
-		//printf("%s\n", result[i]);
-		i++;
-	}
-	//printf("%d", count_word(str, ' '));
-	//printf("%d", count_size(str, ' ', 7));
-	return (0);
-}*/
+// 	result = ft_split(str, '-');
+// 	i = 0;
+// 	while (i < 6)
+// 	{
+// 		printf("%s\n", result[i]);
+// 		i++;
+// 	}
+// 	//printf("%d", count_word(str, ' '));
+// 	//printf("%d", count_size(str, ' ', 7));
+// 	return (0);
+// }
